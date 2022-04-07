f17f9b2465e452a35d6edbb47bb2f82598764129
DavidWalshCode <20276885@studentmail.ul.ie>
2022-04-07 18:15:52 +0100

    Fixing GUI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,9 +2,12 @@
 #include "ui_mainwindow.h"
 #include "Zork.h"
 #include "TextContent.h"
+
 #include <QMessageBox>
-#include <QDebug>
 #include <QTextEdit>
+#include <QDebug>
+#include <QKeyEvent>
+#include <QScrollBar>
 
 //Constructor
 MainWindow::MainWindow(QWidget *parent) :
@@ -12,7 +15,7 @@ MainWindow::MainWindow(QWidget *parent) :
       ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
+    //ui->inputConsole->setFocus();
 }
 
 // Destructor
@@ -21,96 +24,117 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::clearConsole(){
+void MainWindow::clearConsole()
+{
     ui->outputConsole->clear();
 }
 
-// Scrolls to the bottom of the output
-void MainWindow::scrollToBottom(){
-    //ui->scrollArea->verticalScrollBar()->setValue(ui->scrollArea->verticalScrollBar()->maximum());
-}
-
-// Printing to game console - Supports string and QString
-void MainWindow::addStringToConsole(string input){
-    //qDebug("Hello");
+// Printing to console
+void MainWindow::addStringToConsole(const string &input)
+{
+    //qDebug("Does this work?");
     ui->outputConsole->setText(ui->outputConsole->text() + QString::fromStdString("\n") + QString::fromStdString(input));
-    scrollToBottom();
 }
 
-void MainWindow::addQStringToConsole(QString input){
-    //qDebug("Hello");
+// Printing to console
+void MainWindow::addQStringToConsole(const QString &input)
+{
+    //qDebug("Does this qwork?");
     ui->outputConsole->setText(ui->outputConsole->text() + QString::fromStdString("\n") + input);
-    scrollToBottom();
 }
 
-// Clears the console and prints something
-void MainWindow::overwriteConsole(string input){
+// Clears the console and prints
+void MainWindow::overwriteConsole(const string &input)
+{
     ui->outputConsole->clear();
     addStringToConsole(input);
-
-    scrollToBottom();
 }
 
-// Trying to convert input to a command and printing out the appropriate output.
-void MainWindow::parseInput(string input){
-    Command *command = Zork::getParser()->commandConverter(input);
-    overwriteConsole("> " + input + "\n");
-    string output = Zork::processCommand(*command, this);
+// Handling what happens when the "enter" key is pressed on the text edit input.
+void MainWindow::inputTextChanged()
+{
+    // Converting from QString to string and finding the index of "enter" or "\n"
+    string input = ui->inputConsole->toPlainText().toStdString();
+    size_t newlineIndex = input.find('\n');
 
-    // Processes errors
-    if(output.compare("") == 0){
-        //addStringToConsole(Dialogues::inputError);
-        overwriteConsole(TextContent::inputError);
+    // Preventing users from entering several enter lines
+    if (newlineIndex == 0)
+    {
+        ui->inputConsole->clear();
         return;
     }
 
-    //addStringToConsole(output);
-    overwriteConsole(output);
+    // Removing the newline from the input to be later passed into parseInput
+    input = input.substr(0, newlineIndex);
+
+    // Checks if there are any newlines or if the "enter" key is pressed
+    if (newlineIndex != string::npos && input.size() > 0)
+    {
+        this->parseInput(input);
+        this->ui->inputConsole->clear();
+    }
+}
+
+// Trying to convert input to a command and printing out the appropriate output.
+void MainWindow::parseInput(const string &input)
+{
+    Command* command = Zork::getParser()->commandConverter(input);
+
+    string inputString = "> " + input + "\n\n";
+
+    string output = Zork::processCommand(*command);
+
+    // Processes errors
+    if (output.compare("") == 0)
+    {
+        overwriteConsole(inputString + TextContent::inputError);
+        return;
+    }
+
+    overwriteConsole(inputString + output);
 
     delete command;
 
-    ui->input->setFocus();
-    scrollToBottom();
+    ui->inputConsole->setFocus();
 }
 
-
 // Buttons
-void MainWindow::on_northButton_pressed()
+void MainWindow::northButtonPressed()
 {
     parseInput("go north");
 }
 
-void MainWindow::on_eastButton_pressed()
+void MainWindow::eastButtonPressed()
 {
     parseInput("go east");
 }
 
-void MainWindow::on_southButton_pressed()
+void MainWindow::southButtonPressed()
 {
     parseInput("go south");
 }
 
-void MainWindow::on_westButton_pressed()
+void MainWindow::westButtonPressed()
 {
     parseInput("go west");
 }
 
-void MainWindow::on_mapButton_pressed()
+void MainWindow::mapButtonPressed()
 {
     parseInput("map");
 }
 
-void MainWindow::on_infoButton_pressed()
+void MainWindow::infoButtonPressed()
 {
     parseInput("info");
 }
 
-void MainWindow::on_teleportButton_pressed()
+void MainWindow::teleportButtonPressed()
 {
     parseInput("random");
 }
 
-void MainWindow::on_quitButton_pressed()
+void MainWindow::quitButtonPressed()
 {
     // Notes about Message Boxes
     /*
--- mainwindow.h
+++ mainwindow.h
@@ -19,22 +19,21 @@ public:
     ~MainWindow();
 
     void clearConsole();
-    void addStringToConsole(string input);
-    void addQStringToConsole(QString input);
-    void overwriteConsole(string input);
-    void updateBackground(string path);
-    void parseInput(string input);
-    void scrollToBottom();
+    void addStringToConsole(const string &input);
+    void addQStringToConsole(const QString &input);
+    void overwriteConsole(const string &input);
+    void parseInput(const string &input);
 
 private slots:
-    void on_northButton_pressed();
-    void on_eastButton_pressed();
-    void on_southButton_pressed();
-    void on_westButton_pressed();
-    void on_mapButton_pressed();
-    void on_infoButton_pressed();
-    void on_teleportButton_pressed();
-    void on_quitButton_pressed();
+    void northButtonPressed();
+    void eastButtonPressed();
+    void southButtonPressed();
+    void westButtonPressed();
+    void mapButtonPressed();
+    void infoButtonPressed();
+    void teleportButtonPressed();
+    void quitButtonPressed();
+    void inputTextChanged();
 
 private:
     Ui::MainWindow *ui;
