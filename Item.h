7501399a3f8b916dd2c506556c45138adc3482cc
DavidWalshCode <20276885@studentmail.ul.ie>
2022-04-08 11:24:11 +0100

    Moved the exceptions to the top of the class

--- Item.h
+++ Item.h
@@ -12,6 +12,40 @@ using std::exception;
 namespace zork
 {
 
+// Programmer defined exception and inheritance
+// Exception throw when attempting to set the weight of an item with an invalid weight
+class InvalidWeightException : exception
+{
+public:
+
+    InvalidWeightException(int weight) : exception(), weight(weight)
+    {
+    }
+
+    const char* what() const throw ()
+    {
+        return "Weight invalid, must be 0 < weight < 9999";
+    }
+
+    const int weight;
+};
+
+// Exception throw when attempting to set the value of an item with an invalid value
+class InvalidValueException : exception
+{
+public:
+    InvalidValueException(float value) : exception(), value(value)
+    {
+    }
+
+    const char* what() const throw ()
+    {
+        return "Value invalid, must be 0 < value < 9999";
+    }
+
+    const float value;
+};
+
 class Item
 {
 public:
@@ -19,47 +53,18 @@ public:
     Item (string inName);
     string getName();
     string getDescription();
-    int getWeight();
-    void setWeight(int weightGrams);
-    float getValue();
-    void setValue(float value);
 
-    // Virtual function
+    void setWeight(int weightGrams); // can throw InvalidWeightException!
+    void setValue(float value); // can throw InvalidValueException!
+
+    float getValue();
+    int getWeight();
+
+    // Virtual function, in order to define how subclasses of Item behave when used, this method is marked as virtual.
     virtual string use()
     {
         return "Nothing happened...";
     }
-
-    // Programmer defined exception and inheritance
-    class InvalidWeightException : exception
-    {
-    public:
-        InvalidWeightException(int weight) : exception(), weight(weight)
-        {
-        }
-
-        const char* what() const throw ()
-        {
-            return "Weight invalid, must be 0 < weight < 9999";
-        }
-
-        const int weight;
-    };
-
-    class InvalidValueException : exception
-    {
-    public:
-        InvalidValueException(float value) : exception(), value(value)
-        {
-        }
-
-        const char* what() const throw ()
-        {
-            return "Value invalid, must be 0 < value < 9999";
-        }
-
-        const float value;
-    };
 
 protected:
     string name;
@@ -69,10 +74,12 @@ protected:
     bool weaponCheck;
 };
 
+// Example of a subclass of Item
 class Lamp : Item
 {
     Lamp();
 
+    // Define how the Lamp behaves when used, 'overriding' the original behaviour
     string use() override;
 };
 
